356a4581ceac41d75c326ff3c985908b14bf1d68
Chris Smith <chris@chameth.com>
2018-06-05 20:21:37 +0100

    Handle gitea hooks, trigger jenkins.

--- main.py
+++ main.py
@@ -4,27 +4,32 @@ import jenkins
 import requests
 import os
 from bs4 import BeautifulSoup
-from flask import Flask
+from flask import Flask, abort
 
 BASE_URL = os.environ["LAS_BASE_URL"]
 SECRET = os.environ["LAS_SECRET"]
 
-server = jenkins.Jenkins(
+jenkins_server = jenkins.Jenkins(
     os.environ["LAS_JENKINS_URL"],
     username=os.environ["LAS_JENKINS_USER"],
     password=os.environ["LAS_JENKINS_PASSWORD"],
 )
 
 
-def get_hook_url(service, identifier):
+def get_hook_key(service, identifier):
     nonce = (service + SECRET + identifier).encode("ascii")
-    token = hashlib.sha256(nonce).hexdigest()
-    return f"{BASE_URL}hooks/{service}/{identifier}/{token}"
+    return hashlib.sha256(nonce).hexdigest()
+
+
+def get_hook_url(service, identifier):
+    return f"{BASE_URL}hooks/{service}/{identifier}/{get_hook_key(service, identifier)}"
 
 
 def get_jenkins_jobs():
-    for job in server.get_all_jobs():
-        config = BeautifulSoup(server.get_job_config(job["fullname"]), features="xml")
+    for job in jenkins_server.get_all_jobs():
+        config = BeautifulSoup(
+            jenkins_server.get_job_config(job["fullname"]), features="xml"
+        )
         for git_config in config.find_all("scm", class_="hudson.plugins.git.GitSCM"):
             branch_spec = git_config.find("branches").find("name").text
             yield job["fullname"], branch_spec, git_config.find("url").text
@@ -71,6 +76,8 @@ def get_gitea_repos():
         yield repo["full_name"], repo["ssh_url"], repo["clone_url"]
 
 
+repos = dict((name, [ssh, clone]) for name, ssh, clone in get_gitea_repos())
+jobs = list(get_jenkins_jobs())
 app = Flask(__name__)
 
 
@@ -79,4 +86,26 @@ def handle_index():
     return app.send_static_file("index.html")
 
 
-app.run('0.0.0.0')
+@app.route("/hooks/gitea/<path:repo>/<hash>")
+def handle_hook_gitea(repo, hash):
+    print(f"Received hook for repo {repo} with has {hash}")
+    expected_hash = get_hook_key("gitea", repo)
+    if hash != expected_hash:
+        print(f"Hash mismatch: expected {expected_hash}")
+        abort(403)
+
+    if repo not in repos:
+        print(f"Repository not found. Known repos: {repos.keys()}")
+        abort(404)
+
+    urls = repos[repo]
+    for name, spec, url in jobs:
+        if url in urls:
+            # TODO: Check branches
+            print(f"Found matching job: {name} with URL {url}")
+            jenkins_server.build_job(name)
+
+    return "", 204
+
+
+app.run("0.0.0.0")
