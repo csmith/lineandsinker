ac8ae34e3dcbc2218b14090c2a884d2e1f5761cf
Chris Smith <chris@chameth.com>
2019-01-29 18:09:46 +0000

    Allow service creation to fail

--- lineandsinker/services/__init__.py
+++ lineandsinker/services/__init__.py
@@ -7,50 +7,50 @@ from .jenkins import Jenkins
 from .reportbot import ReportBot
 from .slack import Slack
 
+services = {}
 
-def docker_factory():
-    return Docker()
+try:
+    services["docker"] = Docker()
+except:
+    pass
 
-
-def gitea_factory():
-    return Gitea(
+try:
+    services["gitea"] = Gitea(
         os.environ["LAS_GITEA_URL"],
         os.environ["LAS_GITEA_TOKEN"],
         install_hooks="LAS_GITEA_ADD_HOOKS" in os.environ,
     )
+except:
+    pass
 
-def irccat_factory():
-    return IrcCat(
+try:
+    services["irccat"] = IrcCat(
         os.environ["LAS_IRCCAT_HOST"],
         int(os.environ["LAS_IRCCAT_PORT"]),
         os.environ["LAS_IRCCAT_CHANNEL"],
     )
+except:
+    pass
 
-def jenkins_factory():
-    return Jenkins(
+try:
+    services["jenkins"] = Jenkins(
         os.environ["LAS_JENKINS_URL"],
         os.environ["LAS_JENKINS_USER"],
         os.environ["LAS_JENKINS_PASSWORD"],
     )
+except:
+    pass
 
-
-def reportbot_factory():
-    return ReportBot(
+try:
+    services["reportbot"] = ReportBot(
         os.environ["LAS_REPORTBOT_URL"],
         os.environ["LAS_REPORTBOT_KEY"],
         os.environ["LAS_REPORTBOT_CHANNEL"],
     )
+except:
+    pass
 
-
-def slack_factory():
-    return Slack()
-
-
-services = {
-    "docker": docker_factory(),
-    "gitea": gitea_factory(),
-    "irccat": irccat_factory(),
-    "jenkins": jenkins_factory(),
-    "reportbot": reportbot_factory(),
-    "slack": slack_factory(),
-}
+try:
+    services["slack"] = Slack()
+except:
+    pass
